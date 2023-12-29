2d0ec45c8778dc56f47dbe5990193026061c8b62
Olawale <ahmedmolawale@gmail.com>
2023-12-29 02:12:45 +0000

    Add valid android locales

---
+++ src/main/kotlin/com/ahmedmolawale/lokalize/utils/ValidLocales.kt
@@ -0,0 +1,25 @@
+package com.ahmedmolawale.lokalize.utils
+
+val androidSupportedLocales = listOf(
+    "af", "af-rNA", "af-rZA", "am", "am-rET", "ar", "ar-rAE", "ar-rBH", "ar-rDZ", "ar-rEG",
+    "ar-rIQ", "ar-rJO", "ar-rKW", "ar-rLB", "ar-rLY", "ar-rMA", "ar-rOM", "ar-rQA", "ar-rSA",
+    "ar-rSD", "ar-rSY", "ar-rTN", "ar-rYE", "az", "az-rAZ", "be", "be-rBY", "bg", "bg-rBG",
+    "bn", "bn-rBD", "bn-rIN", "bs", "bs-rBA", "ca", "ca-rES", "cs", "cs-rCZ", "da", "da-rDK",
+    "de", "de-rAT", "de-rCH", "de-rDE", "de-rLI", "de-rLU", "dv", "dv-rMV", "el", "el-rGR",
+    "en", "en-rAU", "en-rCA", "en-rGB", "en-rIN", "en-rIE", "en-rNZ", "en-rSG", "en-rUS",
+    "en-rZA", "en-rJW", "es", "es-rAR", "es-rBO", "es-rCL", "es-rCO", "es-rCR", "es-rDO",
+    "es-rEC", "es-rES", "es-rGT", "es-rHN", "es-rMX", "es-rNI", "es-rPA", "es-rPE", "es-rPR",
+    "es-rPY", "es-rSV", "es-rUS", "es-rUY", "es-rVE", "et", "et-rEE", "eu", "eu-rES", "fa",
+    "fa-rIR", "fi", "fi-rFI", "fo", "fo-rFO", "fr", "fr-rBE", "fr-rCA", "fr-rCH", "fr-rFR",
+    "fr-rLU", "fr-rMC", "fr-rSN", "gl", "gl-rES", "gu", "gu-rIN", "he", "he-rIL", "hi",
+    "hi-rIN", "hr", "hr-rBA", "hr-rHR", "hu", "hu-rHU", "hy", "hy-rAM", "id", "id-rID", "is",
+    "is-rIS", "it", "it-rCH", "it-rIT", "ja", "ja-rJP", "ka", "ka-rGE", "kk", "kk-rKZ", "km",
+    "km-rKH", "kn", "kn-rIN", "ko", "ko-rKR", "ky", "ky-rKG", "lo", "lo-rLA", "lt", "lt-rLT",
+    "lv", "lv-rLV", "mk", "mk-rMK", "ml", "ml-rIN", "mn", "mn-rMN", "mr", "mr-rIN", "ms",
+    "ms-rMY", "nb", "nb-rNO", "ne", "ne-rNP", "nl", "nl-rBE", "nl-rNL", "no", "no-rNO", "no-rNY",
+    "pl", "pl-rPL", "pt", "pt-rBR", "pt-rPT", "ro", "ro-rRO", "ru", "ru-rRU", "si", "si-rLK",
+    "sk", "sk-rSK", "sl", "sl-rSI", "sq", "sq-rAL", "sr", "sr-rBA", "sr-rCS", "sr-rME", "sr-rRS",
+    "sv", "sv-rSE", "sw", "sw-rKE", "ta", "ta-rIN", "te", "te-rIN", "th", "th-rTH", "tl", "tl-rPH",
+    "tr", "tr-rTR", "ug", "ug-rCN", "uk", "uk-rUA", "ur", "ur-rPK", "uz", "uz-rUZ", "vi", "vi-rVN",
+    "zh", "zh-rCN", "zh-rHK", "zh-rMO", "zh-rSG", "zh-rTW"
+)
